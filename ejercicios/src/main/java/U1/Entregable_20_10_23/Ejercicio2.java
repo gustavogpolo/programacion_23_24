54ac5c0f4135d6850e7d73ed80fdbfb4fe2d2b2f
gustavogpolo <ggalpol491@g.educaand.es>
2023-10-20 11:30:26 +0200

    Entegable UD 1

---
+++ ejercicios/src/main/java/U1/Entregable_20_10_23/Ejercicio2.java
@@ -0,0 +1,39 @@
+package U1.Entregable_20_10_23;
+
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce un número entero positivo: ");
+        int numero = scanner.nextInt();
+        if (numero <= 0) {
+            System.out.println("El número debe ser positivo. Por favor, inténtalo de nuevo.");
+            return;
+        }
+        System.out.print("Introduce una cifra entre 0 y 9: ");
+        int cifra = scanner.nextInt();
+        if (cifra < 0 || cifra > 9) {
+            System.out.println("La cifra debe estar en el rango de 0 a 9. Por favor, inténtalo de nuevo.");
+            return;
+        }
+        scanner.close();
+        String numeroStr = String.valueOf(numero);
+        int count = 0;
+        System.out.println("Ocurrencias de la cifra " + cifra + " en el número " + numero + ":");
+        for (int i = 0; i < numeroStr.length(); i++) {
+            char digito = numeroStr.charAt(i);
+            if (digito - '0' == cifra) {
+                count++;
+                System.out.println("Posición " + (i + 1));
+            }
+
+            if (count == 0) {
+                System.out.println("La cifra " + cifra + " no se encontró en el número " + numero);
+            } else {
+                System.out.println("Total de ocurrencias: " + count);
+            }
+        }
+    }
+}
---
+++ ejercicios/src/main/java/U1/Entregable_20_10_23/Ejercicio5.java
@@ -0,0 +1,47 @@
+package U1.Entregable_20_10_23;
+
+import java.util.Scanner;
+
+public class Ejercicio5 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+
+        System.out.println("Introduzca un numero entero: ");
+        int numero = teclado.nextInt();
+
+
+        System.out.println("Introduzca una cifra: (Entre el 0 y el 9 ambos inclusive)");
+        int cifra = teclado.nextInt();
+
+
+        String numeroString = Integer.toString(numero);
+
+
+        int ocurrencias = 0;
+        String posiciones = "";
+
+
+        for (int i = 0; i < numeroString.length(); i++) {
+
+
+            int digito = Character.getNumericValue(numeroString.charAt(i));
+
+
+            if (digito == cifra) {
+                ocurrencias++;
+
+
+                posiciones += (i + 1) + " - ";
+            }
+        }
+
+
+        posiciones = posiciones.substring(0, posiciones.length() - 2);
+
+
+        System.out.println("Resultado:");
+        System.out.println(ocurrencias + " ocurrencias");
+        System.out.println("Posiciones: " + posiciones);
+    }
+}
